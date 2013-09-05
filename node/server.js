a85fab9d2e50a69a3e74c2e8276d9f8acaa9def9
mbreton <mbreton@xebia.fr>
2013-09-05 23:52:35 +0200

    Node : Fix query problem but return NaN

--- node/server.js
+++ node/server.js
@@ -3,39 +3,47 @@ var express = require('express');
 
 var app = express();
 
-// ex : S=100&K=102&u=1.07&R=1.01&expiration=1
+// http://localhost:8080/price?S=100&K=102&U=1.07&R=1.01&expiration=1
 app.get('/price', function(req, res) {
+
+    var s = +req.query.S,          // initial price
+        k = +req.query.K,          // constractuel price
+        r = +req.query.R,          // probability that the price up or down in each node
+        u = +req.query.U,          // coef used to determinate the next value of S for the two sub-nodes
+        n = +req.query.expiration; // number of unit time (month for example) which define the tree depth
 
     console.log(req.query);
 
-    var s = req.query.S,          // initial price
-        k = req.query.K,          // whished price
-        r = req.query.R,          // probability that the price up or down in each node
-        u = req.query.U,          // coef used to determinate the next value of S for the two sub-nodes
-        n = req.query.expiration; // number of unit time (month for example) which define the tree depth
+    var values= "S="+s+" K="+k+" R="+r+" U="+u+" N="+n;
+    console.log(values);
 
     if (!s || !k || !r || !n || !u){
-        res.send("S, K, R or expiration is undefined or is zero<br/>"+
-            "S="+s+" K="+k+" R="+r+" u="+u+" n="+n);
+        res.send("S, K, R, U, or expiration is undefined or is zero<br/>"+values);
     } else {
 
         var d = 1 / u;              // coef when the price down
         var q = (r - d) / (u - d);
 
-        var result = 1/Math.pow(R, expiration) * recusiveSum(n, 0, q, s, u);
-        var roundedResult = Math.round(result * 100) / 100;
+        console.log('d='+d+' q='+q);
 
-        req.response.end(roundedResult);
+        var result = 1/Math.pow(r, n) * recusiveSum(n, 0, q, s, u);
+        var roundedResult = Math.round(result * 100) / 100;
+        console.log(roundedResult)
+
+        res.end(roundedResult);
     }
 });
 
-var recusiveSum = function (n, k, q, s, u){
-    var r = binom(n, k) * Math.pow(q,n-k) * Math.pow(1-q,k) * (s * Math.pow(u, n-k) * Math.pow(d, k));
+var recusiveSum = function (n, k, q, s, u, d){
+    var result =0,
+        r = binom(n, k) * Math.pow(q,n-k) * Math.pow(1-q,k) * (s * Math.pow(u, n-k) * Math.pow(d, k));
     if (k == n){
-        return Math.max(0, s - r)
+        result = Math.max(0, s - r)
     } else{
-        return r + recusiveSum(n, k+1, q, s, u);
+        result = r + recusiveSum(n, k+1, q, s, u, d);
     }
+    console.log("When k="+k+" result is equal to "+r);
+    return result;
 };
 
 // binomial coef calculation
